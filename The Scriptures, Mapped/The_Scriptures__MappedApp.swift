667746381f9783a39997cf1b6769bcd7346c6667
Carlos Evelo <carlosevelo96@gmail.com>
2022-11-28 20:06:47 -0700

    Initial Commit

---
+++ The Scriptures, Mapped/The_Scriptures__MappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Scriptures__MappedApp.swift
+//  The Scriptures, Mapped
+//
+//  Created by Carlos Evelo on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Scriptures__MappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
